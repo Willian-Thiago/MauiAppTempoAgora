d9a2bd4b4342ab054308faf54d261ce9487f28a1
willi <willi@LAPTOP-NGU55PT5>
2025-04-06 20:01:09 -0300

    Integração com a API weather, tratamento de erros de digitação, falta de conexão, formatação dos dados exibidos

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,81 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Net.NetworkInformation;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            if (!string.IsNullOrEmpty(txt_cidade.Text))
+            {
+                // Verificando a conexão com a internet de forma mais robusta
+                if (!VerificarConexao())
+                {
+                    // Certificando-se de que o DisplayAlert é chamado na thread principal
+                    MainThread.BeginInvokeOnMainThread(async () =>
+                    {
+                        await DisplayAlert("Alerta", "Você está sem conexão com a internet. Verifique sua conexão e tente novamente.", "Ok");
+                    });
+                    return; // Interrompe a execução do método se não houver conexão
+                }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+                try
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat}º \n " +
+                                         $"Longitude: {t.lon}º \n " +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do vento: {t.speed} m/s\n" +
+                                         $"Visibilidade: {t.visibility} Km \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n " +
+                                         $"Por do Sol: {t.sunset} \n " +
+                                         $"Temp Máx: {t.temp_max}º \n " +
+                                         $"Temp Mín: {t.temp_min}º \n ";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        await DisplayAlert("Aviso", "Cidade não encontrada. Por favor, verifique o nome da cidade.", "Ok");
+                        lbl_res.Text = "Dados da Previsão.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Ops", "Ocorreu um erro inesperado: " + ex.Message, "Ok");
+                    lbl_res.Text = "Erro ao buscar a previsão.";
+                }
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                lbl_res.Text = "Preencha a cidade.";
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        // Método de verificação de conexão diretamente na MainPage
+        private bool VerificarConexao()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
-
 }
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,82 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+using System.Net.NetworkInformation; // Adicionando namespace para verificar a conexão
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "dcbe9d56f0b513005f440e27aab0f826";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={cidade}&units=metric&appid={chave}&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                try // Adicionando um bloco try-catch para lidar com exceções de rede
+                {
+
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+
+                    // Verificando o código de status da resposta HTTP
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+                        var rascunho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                        DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                        //Obtendo a visibilidade em metros e convertendo para km
+                        int visibilidadeEmMetros = (int)rascunho["visibility"];
+                        double visibilidadeEmQuilometros = visibilidadeEmMetros / 1000.0;
+
+                        t = new()
+                        {
+                            lat = (double)rascunho["coord"]["lat"],
+                            lon = (double)rascunho["coord"]["lon"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            speed = (double)rascunho["wind"]["speed"],
+
+                            // armazenando a visibilidade convertida em km
+                            visibility = (int)Math.Round(visibilidadeEmQuilometros),
+
+                            sunrise = sunrise.ToString("HH:mm"),
+                            sunset = sunset.ToString("HH:mm"),
+                        }; // Fecha o objeto do Tempo
+                    }
+                    // Verificando se o código de status indica que a cidade não foi encontrada (HTTP 404)
+                    else if (resp.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        // Retorna null para sinalizar que a cidade não foi encontrada
+                        return null;
+                    }
+                    // Outros códigos de erro podem ser tratados aqui, se necessário
+                    else
+                    {
+                        // Para outros erros, também retornamos null
+                        return null;
+                    }
+                }
+                // Captura exceções que podem ocorrer durante a comunicação HTTP, como falta de internet
+                catch (HttpRequestException)
+                {
+                    // Retorna null para sinalizar que houve um erro de conexão
+                    return null;
+                }               
+            } // fecha laço using
+             
+            return t;
+        }       
+    }
+}
